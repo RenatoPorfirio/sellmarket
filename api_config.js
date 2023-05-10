56a890dfc2dd3869fcd569230aa6d371659887e6
renato <renatoporfiriox@gmail.com>
2023-05-10 12:44:18 +0000

    erro corrigido

--- api_config.js
+++ api_config.js
@@ -13,7 +13,7 @@ api.post('/cadastro/fornecedor', (requisicao, resposta) => {
   const ip = requisicao.headers['x-forwarded-for'] || requisicao.socket.remoteAddress;
   let fornecedor = requisicao.body;
   if(fornecedor === undefined || !fornecedor){
-    reposta.send('Campo vazio. Nenhum fornecedor foi cadastrado.');
+    resposta.send('Campo vazio. Nenhum fornecedor foi cadastrado.');
   }
   else{
     resposta.send('Fornecedor cadastrado com sucesso!');
