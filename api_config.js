c8e995890641749586a91619f2e010ba162af5b1
renato <renatoporfiriox@gmail.com>
2023-05-10 02:31:16 +0000

    buscar produto adicionada

--- api_config.js
+++ api_config.js
@@ -8,7 +8,8 @@ api.get('/', (requisicao, resposta) => {
   console.log('<'+ip+'>', requisicao.method, requisicao.url, resposta.statusCode);
 });
 
-api.get('/pesquisa/fornecedor', (requisicao, resposta) => {
+//buscar fornecedor
+api.get('/busca/fornecedor', (requisicao, resposta) => {
   const ip = requisicao.headers['x-forwarded-for'] || requisicao.socket.remoteAddress;
   try{
     let id = requisicao.query.id;
@@ -21,6 +22,44 @@ api.get('/pesquisa/fornecedor', (requisicao, resposta) => {
         console.log('<'+ip+'>', 'Conexao bem sucedida.');
   
         conexao.query('select * from fornecedor where id_fornecedor = '+id, (erro, resultado, campos) => {
+          if(erro) throw erro;
+          resposta.send(JSON.stringify(resultado[0], null, 3));
+        });
+        console.log('<'+ip+'>', requisicao.method, requisicao.url, resposta.statusCode);
+        conexao.release();
+        console.log('<'+ip+'>', 'Conexão com o banco de dados encerrada.');
+      });
+    }
+    catch{
+      console.log('Erro ao tentar conectar-se ao banco de dados!');
+      resposta
+        .status(500)
+        .send(JSON.stringify({estado: 500, mensagem: 'Houve um problema na conexão com o banco de dados. Desculpe o transtorno!'}), null, 3);
+      console.log('<'+ip+'>', requisicao.method, requisicao.url, resposta.statusCode);
+    }
+  }
+  catch{
+      resposta
+          .status(500)
+          .send(JSON.stringify({estado: 500, mensagem: 'Houve um problema com a requisicao. Verifique a documentacao da api e tente novamente.'}), null, 3);
+        console.log('<'+ip+'>', requisicao.method, requisicao.url, resposta.statusCode);
+  }
+});
+
+//buscar produto
+api.get('/busca/produto', (requisicao, resposta) => {
+  const ip = requisicao.headers['x-forwarded-for'] || requisicao.socket.remoteAddress;
+  try{
+    let cod = requisicao.query.cod;
+    if(cod === undefined)
+      throw('');
+    try{
+      pool.getConnection((erro, conexao) => {
+        console.log('<'+ip+'>', 'Solicitacao de conexao com o banco de dados.');
+        if (erro) throw erro;
+        console.log('<'+ip+'>', 'Conexao bem sucedida.');
+  
+        conexao.query('select * from produto where cod_produto = '+cod, (erro, resultado, campos) => {
           if(erro) throw erro;
           resposta.send(JSON.stringify(resultado[0], null, 3));
         });
